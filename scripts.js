51fbf1174f073186962ccb062f877dfbe20d1ed7
BorjaEA <borjaestevez98@hotmail.com>
2023-04-20 10:30:52 +0200

    añadido el color

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 
 var filas = parseInt(prompt("Ingrese el número de filas:"));
-var columnaInicial = parseInt(prompt("Ingrese la columna inicial:"));
 
 var piramide = "";
 
@@ -21,5 +20,23 @@ for (var fila = 0; fila < filas; fila++) {
 var numeroDiv = parseInt(prompt("Elige un número entre 1 y 5:"));
 
 var divResultado = document.getElementById(numeroDiv.toString());
+
+var numeroColor = parseInt(prompt("Elige un número para el color del texto (1 = rojo, 2 = verde, 3 = azul):"));
+
+
+switch (numeroColor) {
+  case 1:
+    divResultado.style.color = "red";
+    break;
+  case 2:
+    divResultado.style.color = "green";
+    break;
+  case 3:
+    divResultado.style.color = "blue";
+    break;
+  default:
+    divResultado.style.color = "black"; 
+    break;
+}
 
 divResultado.innerHTML = piramide;
